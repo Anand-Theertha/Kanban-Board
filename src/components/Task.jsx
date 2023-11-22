e28e69fdfa0897bc393def16c60bf230992f176d
Anand-Theertha <54447234+Anand-Theertha@users.noreply.github.com>
2023-11-22 12:15:07 +0530

    feat:Delete Tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,25 @@
 import classNames from "classnames";
 import "./Task.css";
 import { useStore } from "../store";
+import trash from "../assets/trash-2.svg";
 
 const Task = ({ title }) => {
   const task = useStore((store) =>
     store.tasks.find((task) => task.title === title)
   );
 
+  const deleteTasks = useStore((store) => store.deleteTask);
+
   return (
     <div className="task">
       <div>{task.title}</div>
       <div className="bottomWrapper">
-        <div></div>
+        <div>
+          <img
+            src={trash} alt="Trash Icon"
+            onClick={() => deleteTasks(task.title)}
+          />
+        </div>
         <div className={classNames("status", task.state)}>{task.state}</div>
       </div>
     </div>
